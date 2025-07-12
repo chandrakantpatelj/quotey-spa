6e26926b236b8020c6f8ba19766f74f7bdd7212d
Pratik-Shemare-JSPIT <pratik@jspinfotech.com>
2025-07-12 11:38:44 +0530

    commit

--- src/app/ReduxProviders.js
+++ src/app/ReduxProviders.js
@@ -1,9 +1,37 @@
 'use client'
 
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
-
 import { store } from '@/redux/store'
 
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+import awsExports from '@/components/auth/awsexport'
+import { components, formFields } from '@/components/auth/AuthenticatorUI'
+
+let isConfigured = false
+
 export default function ReduxProviders({ children }) {
-  return <Provider store={store}>{children}</Provider>
+  useEffect(() => {
+    if (!isConfigured) {
+      Amplify.configure(awsExports)
+      isConfigured = true
+    }
+  }, [])
+
+  return (
+    // <Authenticator formFields={formFields} components={components} hideSignUp={true}>
+    //   {({ signOut, user }) => <Provider store={store}>{children}</Provider>}
+    // </Authenticator>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <h1>Hello, {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
+  )
 }
---
+++ src/components/auth/awsexport.js
@@ -0,0 +1,10 @@
+const awsExports = {
+  Auth: {
+    region: 'ap-southeast-2',
+    userPoolId: 'ap-southeast-2_TZOh1RKDU',
+    userPoolWebClientId: '2b9kvr9oj8b44uit2k8co2fde9',
+    mandatorySignIn: false
+  }
+}
+
+export default awsExports
