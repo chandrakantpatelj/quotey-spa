e0bdf469b828b1f9cc6d87fec1637363ee59b946
mauliksuvagiya <maulik@jspinfotech.com>
2025-07-16 18:56:05 +0530

    Add new dashboard pages and update logo component
    - Created new pages: Calendar, Maintenance, Reports, S Drive, Sales Dashboard, Campaigns, HL Package, Land, Leads, Quotations, and Job.
    - Updated Logo component to use a new logo image.
    - Enhanced VerticalMenu with a Sales submenu containing relevant links.
    - Added CardActionsTable component for displaying action cards.

---
+++ src/app/(dashboard)/s-drive/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SDrive() {
+  return <div>S Drive</div>
+}
+
+export default SDrive
---
+++ src/app/(dashboard)/sales/sales-dashboard/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SalesDashBoard() {
+  return <div>SalesDashBoard</div>
+}
+
+export default SalesDashBoard
